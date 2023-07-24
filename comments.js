a5baffe7726f2d452d192f3f512c85e21cb42d23
RChernyak <roman.chernyak@teva.ru>
2023-07-24 10:25:34 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,12 @@
 // create web server
+const http = require("http");
 
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
